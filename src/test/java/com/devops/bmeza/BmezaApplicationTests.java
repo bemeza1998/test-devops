973ba19ea9d0f883c97d0a4853ddcb4f3c76d1d2
bemeza <esteban-meza85@hotmail.com>
2024-09-24 21:32:20 -0500

    Firts commit

---
+++ src/test/java/com/devops/bmeza/BmezaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.bmeza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BmezaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
